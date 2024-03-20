4123b97215fd32318d27bf5eadad1e2c207afe14
Agustin Repicio <agus.mach4@gmail.com>
2024-03-20 08:59:47 -0300

    utilizando comando END

--- imprimir.php
+++ imprimir.php
@@ -4,6 +4,10 @@
     <h1> Mi segundo archivo PHP </h1>
     <body>
     <?php
+
+    // "END" funciona como un "echo"
+    // sirve para no repetir "echo"
+    // y ingresar texto :)
     echo <<<END
     en un mundo conectado,
     donde cada segundo se envia millones de correos 
