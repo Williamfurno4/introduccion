7f4e4b0cc95c279c0a6a27fdc798f80f70d9ab76
Agustin Repicio <agus.mach4@gmail.com>
2024-03-20 09:11:10 -0300

    utilizando comando END y comentarios

--- imprimir.php
+++ imprimir.php
@@ -5,9 +5,14 @@
     <body>
     <?php
 
-    // "END" funciona como un "echo"
-    // sirve para no repetir "echo"
-    // y ingresar texto :)
+    // hay 2 tipos de comentarios el // y el /* */
+    // con ctrl + c+k comentas todo lo copiado :)
+    /* "END" funciona como un "echo"
+       sirve para no repetir "echo"
+       y ingresar texto :) 
+    */
+
+
     echo <<<END
     en un mundo conectado,
     donde cada segundo se envia millones de correos 
