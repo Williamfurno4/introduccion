ff0b1b26615c310d60557009dc10da8d2071be3c
Agustin Repicio <agus.mach4@gmail.com>
2024-03-20 08:56:26 -0300

    segundo trabajo

---
+++ imprimir.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+
+    <h1> Mi segundo archivo PHP </h1>
+    <body>
+    <?php
+    echo <<<END
+    en un mundo conectado,
+    donde cada segundo se envia millones de correos 
+    electronicos...
+    END; ?>
+    </body>
+</html>
